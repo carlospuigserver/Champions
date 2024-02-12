d42cd3d05d6928e27e470abbe56a6df4262cf6f8
carlospuigserver <cpuiggut@myuax.com>
2024-02-12 17:25:36 +0100

    c

---
+++ 23-24/PYTHON/datos.py
@@ -0,0 +1,104 @@
+import sqlite3
+
+# Conectarse a la base de datos nueva con el nombre especificado
+conn = sqlite3.connect('22-23/SQL/champions(22-23).db')
+cursor = conn.cursor()
+
+# Crear la tabla de la fase de grupos
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS fase_de_grupos (
+        id_partido INTEGER PRIMARY KEY AUTOINCREMENT,
+        equipo_local TEXT,
+        equipo_visitante TEXT,
+        resultado_final TEXT
+    )
+''')
+
+# Crear las tablas de octavos, cuartos, semis y final
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS octavos (
+        id_partido INTEGER PRIMARY KEY,
+        equipo_local TEXT,
+        equipo_visitante TEXT,
+        resultado_ida TEXT,
+        resultado_vuelta TEXT
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS cuartos (
+        id_partido INTEGER PRIMARY KEY,
+        equipo_local TEXT,
+        equipo_visitante TEXT,
+        resultado_ida TEXT,
+        resultado_vuelta TEXT
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS semis (
+        id_partido INTEGER PRIMARY KEY,
+        equipo_local TEXT,
+        equipo_visitante TEXT,
+        resultado_ida TEXT,
+        resultado_vuelta TEXT
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS final (
+        id_partido INTEGER PRIMARY KEY,
+        equipo_local TEXT,
+        equipo_visitante TEXT,
+        resultado_final TEXT
+    )
+''')
+
+
+# Lista de partidos y resultados de la fase de grupos para insertar en la base de datos
+group_stage_matches = [
+    
+
+]
+# Insertar datos en la tabla de la fase de grupos
+def insert_group_stage_matches(cursor, matches):
+    cursor.executemany('''
+        INSERT INTO fase_de_grupos (equipo_local, equipo_visitante, resultado_final)
+        VALUES (?, ?, ?)
+    ''', matches)
+
+# Insert data into 'octavos' table
+octavos_matches = [
+    
+]
+
+# Insert data into 'cuartos' table
+cuartos_matches = [
+   
+]
+
+
+# Insert data into 'semis' table
+semis_matches = [
+    
+]
+
+# Insert data into 'final' table
+final_match = [
+    
+        
+    ]
+
+# Insertar los datos en las tablas correspondientes
+insert_group_stage_matches(cursor, group_stage_matches)
+cursor.executemany('INSERT INTO octavos VALUES (?,?,?,?,?)', octavos_matches)
+cursor.executemany('INSERT INTO cuartos VALUES (?,?,?,?,?)', cuartos_matches)
+cursor.executemany('INSERT INTO semis VALUES (?,?,?,?,?)', semis_matches)
+cursor.executemany('INSERT INTO final VALUES (?,?,?,?)', final_match)
+
+conn.commit()
+conn.close()
+
+# Ruta al archivo de la base de datos
+db_file_path = '22-23/SQL/champions(22-23).db'
+print(db_file_path)
