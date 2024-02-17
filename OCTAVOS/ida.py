69628d598cbaed729f4ee779e44fae46472cf89c
carlospuigserver <cpuiggut@myuax.com>
2024-02-17 01:24:40 +0100

    comit

---
+++ OCTAVOS/ida.py
@@ -0,0 +1,217 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+# Mapeo de equipos proporcionado
+mapeo_equipos = {
+    'Man City': 'Manchester City',
+    'Paris': 'Paris Saint-Germain',
+    'FC Porto': 'Porto',
+    'FCSB': 'Steaua Bucharest',
+    'CSKA Moskva': 'CSKA Moscow',
+    'Shakhtar Donetsk': 'Shakhtar',
+    'BATE Borisov': 'BATE',
+    'Sporting CP': 'Sporting Lisbon',
+    'APOEL': 'APOEL Nicosia',
+    'Malmö': 'Malmo FF',
+    'Ludogorets': 'Ludogorets Razgrad',
+    'Dynamo Kyiv': 'Dynamo Kiev',
+    'Olympiacos': 'Olympiakos',
+    'Mönchengladbach': 'Borussia Monchengladbach',
+    'Galatasaray': 'Galatasaray A.S.',
+    'Zenit': 'Zenit Saint Petersburg',
+    'Dinamo Zagreb': 'Dinamo Zagreb',
+    'Legia': 'Legia Warsaw',
+    'Rostov': 'FC Rostov',
+    'Beşiktaş': 'Besiktas',
+    'Leicester': 'Leicester City',
+    'Bayern München': 'Bayern Munich',
+    'Bayern': 'Bayern Munich',  
+    'Qarabağ': 'Qarabag',
+    'Atlético Madrid': 'Atletico Madrid',
+    'Atlético': 'Atletico Madrid',
+    'Spartak Moskva': 'Spartak Moscow',
+    'Inter Milan': 'Inter',
+    'Inter': 'Inter',
+    'Lyon': 'Olympique Lyonnais',
+    'Nápoles': 'Napoli',
+    'Salzburgo': 'RB Salzburg',
+    'Estrella Roja': 'Red Star Belgrade',
+    'Lokomotiv Moscú': 'Lokomotiv Moscow',
+    'Genk': 'KRC Genk',
+    'Crvena zvezda': 'Red Star Belgrade',
+    'RB Leipzig': 'RB Leipzig',
+    'Slavia Praga': 'Slavia Prague',
+    'Bayer Leverkusen': 'Bayer 04 Leverkusen',
+    'Valencia': 'Valencia CF',
+    'Chelsea': 'Chelsea FC',
+    'Sevilla': 'Sevilla FC',
+    'Rennes': 'Stade Rennais FC',
+    'Krasnodar': 'FC Krasnodar',
+    'İstanbul Başakşehir': 'Istanbul Basaksehir',
+    'Marsella': 'Olympique de Marseille',
+    'Midtjylland': 'FC Midtjylland',
+    'Ferencváros': 'Ferencvarosi TC',
+    'Club Brugge': 'Club Brugge KV',
+    'Royal Antwerp': 'Royal Antwerp FC',
+    'Union Berlin': 'Union Berlin',
+    'Newcastle': 'Newcastle United',
+    'Feyenoord': 'Feyenoord Rotterdam',
+    'Celtic': 'Celtic FC',
+    'PSV Eindhoven': 'PSV Eindhoven',
+    'Lens': 'RC Lens',
+    'Braga': 'SC Braga',
+    'Salzburg': 'RB Salzburg',
+    'Real Sociedad': 'Real Sociedad',
+    'Copenhagen': 'FC Copenhagen',
+    # Añadiendo mapeos adicionales para nombres alternativos encontrados en la lista
+    'AEK Athens': 'AEK Athens',
+    'AFC Ajax': 'Ajax',
+    'Ajax': 'Ajax',
+    'Anderlecht': 'Anderlecht',
+    'Arsenal': 'Arsenal',
+    'Astana': 'Astana',
+    'Atalanta': 'Atalanta',
+    'Athletic Club': 'Athletic Bilbao',  # Asumiendo que es Athletic Bilbao
+    'Atletico Madrid': 'Atletico Madrid',
+    'Atlético de Madrid': 'Atletico Madrid',
+    'Barcelona': 'FC Barcelona',
+    'Basel': 'FC Basel',
+    'Benfica': 'SL Benfica',
+    'Borussia Dortmund': 'Borussia Dortmund',
+    'Brugge': 'Club Brugge KV',
+    'Maccabi Haifa': 'Maccabi Haifa',
+    'Besiktas': 'Besiktas',
+    'Schalke': 'FC Schalke 04',
+    'Viktoria Plzeň': 'Viktoria Plzen',
+    'Dortmund': 'Borussia Dortmund',
+    'Maccabi Tel Aviv': 'Maccabi Tel Aviv',
+    'Leverkusen': 'Bayer Leverkusen',
+    'Liverpool': 'Liverpool',
+    'Villarreal': 'Villarreal',
+    'Milan': 'Milan',
+    'Milán': 'Milan',
+    'AC Milan': 'Milan',
+    'Tottenham': 'Tottenham Hotspur',
+    'Roma': 'AS Roma',
+    'Inter de Milán': 'Inter Milan',
+    'PSV': 'PSV Eindhoven',
+    'PSG': 'Paris Saint-Germain',
+    'Maribor': 'NK Maribor',
+    'Gent': 'KAA Gent',
+    'Manchester City': 'Manchester City',
+    'Kobenhavn': 'FC Copenhagen',
+    'Lokomotiv Moskva': 'Lokomotiv Moscow',
+    'Red Bull Salzburg': 'RB Salzburg',
+    'Real Madrid': 'Real Madrid',
+    'Schalke 04': 'FC Schalke 04',
+    'Napoli': 'SSC Napoli',
+    'Liverpool FC': 'Liverpool',
+    'Lille': 'Lille OSC',
+    'Crvena Zvezda': 'Red Star Belgrade',
+    'Olympique de Marseille': 'Olympique Marseille',
+    'Tottenham Hotspur': 'Tottenham Hotspur',
+    'Borussia Mönchengladbach': 'Borussia Monchengladbach',
+    'FC Barcelona': 'FC Barcelona',
+    'Porto': 'FC Porto',
+    'Rangers': 'Rangers FC',
+    'Man United': 'Manchester United',
+    'Manchester United': 'Manchester United',
+    'Bayern Munich': 'FC Bayern Munich',
+    'Dinamo Kiev': 'Dynamo Kyiv',
+    'Bayern Múnich': 'FC Bayern Munich',
+    'Red Star Belgrade': 'Red Star Belgrade',
+    'Sheriff Tiraspol': 'Sheriff Tiraspol',
+    'Juventus': 'Juventus FC',
+    'Marseille': 'Olympique Marseille',
+    'Monaco': 'AS Monaco',
+    'Paris Saint-Germain': 'Paris Saint-Germain',
+    'Sporting de Portugal': 'Sporting Lisbon',
+    'Eintracht Frankfurt': 'Eintracht Frankfurt',
+    'Mónaco': 'AS Monaco',
+    'Lazio': 'SS Lazio',
+    'Austria Wien': 'Austria Vienna',
+    'Wolfsburg': 'VfL Wolfsburg',
+    'Hoffenheim': 'TSG 1899 Hoffenheim',
+    'Young Boys': 'BSC Young Boys',
+    # Continuar con el mapeo para el resto de equipos según sea necesario
+}
+
+# Función para aplicar el mapeo a los nombres de los equipos
+def unificar_nombres(equipo):
+    return mapeo_equipos.get(equipo, equipo)
+
+# Cargar y procesar datos de temporadas anteriores
+def cargar_y_procesar_temporada(ruta_archivo):
+    df_temp = pd.read_csv(ruta_archivo)
+    df_temp['equipo_local'] = df_temp['equipo_local'].apply(unificar_nombres)
+    df_temp['equipo_visitante'] = df_temp['equipo_visitante'].apply(unificar_nombres)
+    return df_temp
+
+# Lista de rutas de archivos CSV para temporadas anteriores
+archivos_temporadas = [
+    '13-14/13-14.csv', '14-15/14-15.csv', '15-16/15-16.csv', '16-17/16-17.csv', 
+    '17-18/17-18.csv', '18-19/18-19.csv', '19-20/19-20.csv', '20-21/20-21.csv', 
+    '21-22/21-22.csv', '22-23/22-23.csv'
+]
+
+df_todas_temporadas = pd.concat([cargar_y_procesar_temporada(archivo) for archivo in archivos_temporadas])
+df_todas_temporadas.dropna(subset=['resultado_ida', 'equipo_local', 'equipo_visitante'], inplace=True)
+
+# Cargando los datos específicos de la temporada 23-24
+ruta_archivo_23_24 = '23-24/23-24.csv'
+df_23_24 = pd.read_csv(ruta_archivo_23_24)
+df_23_24['equipo_local'] = df_23_24['equipo_local'].apply(unificar_nombres)
+df_23_24['equipo_visitante'] = df_23_24['equipo_visitante'].apply(unificar_nombres)
+
+# Filtrando para octavos de final de la temporada 23-24
+df_23_24_octavos = df_23_24[df_23_24['fase'] == 'octavos']
+
+# Preparación de los datos para el modelo
+X = df_todas_temporadas[['equipo_local', 'equipo_visitante']]
+y = df_todas_temporadas['resultado_ida']
+
+# División de los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Creando el pipeline con OneHotEncoder y RandomForestClassifier
+column_transformer = ColumnTransformer([
+    ('one_hot', OneHotEncoder(handle_unknown='ignore'), ['equipo_local', 'equipo_visitante'])
+], remainder='passthrough')
+
+pipeline = Pipeline([
+    ('one_hot_encoder', column_transformer),
+    ('classifier', RandomForestClassifier(n_estimators=100, random_state=42))
+])
+
+# Entrenamiento del modelo
+pipeline.fit(X_train, y_train)
+
+# Preparando los datos de octavos para la predicción
+X_23_24_octavos = df_23_24_octavos[['equipo_local', 'equipo_visitante']]
+
+# Realizando predicciones
+predicciones_ida_23_24 = pipeline.predict(X_23_24_octavos)
+
+# Crear un DataFrame temporal para las predicciones
+df_predicciones = pd.DataFrame({
+    'id_partido': df_23_24_octavos['id_partido'].values,
+    'resultado_ida_pred': predicciones_ida_23_24
+})
+
+# Fusionar el DataFrame original de la temporada 23-24 con las predicciones
+df_23_24 = pd.merge(df_23_24, df_predicciones, on='id_partido', how='left')
+
+# Actualizar solo las filas de los octavos de final con las predicciones de 'resultado_ida'
+df_23_24.loc[df_23_24['fase'] == 'octavos', 'resultado_ida'] = df_23_24.loc[df_23_24['fase'] == 'octavos', 'resultado_ida_pred']
+
+# Eliminar la columna temporal de predicciones
+df_23_24.drop(columns=['resultado_ida_pred'], inplace=True)
+
+# Guardar el DataFrame actualizado en un nuevo archivo CSV
+df_23_24.to_csv('OCTAVOS/OctavosIda.csv', index=False)
+
+print("El archivo 'OctavosIda.csv' ha sido guardado correctamente. Solo los resultados de ida de los octavos de final han sido actualizados con las predicciones.")
